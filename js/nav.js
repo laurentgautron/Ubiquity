2f83a1603bfddf71f49675288190171ef5a3042e
laurent GAUTRON <laurentgautron@protonmail.com>
2021-04-12 23:04:13 +0200

    hamburger menu

--- js/nav.js
+++ js/nav.js
@@ -1,6 +1,7 @@
 const toogleMenu = document.querySelector('.toogle-menu');
 const mainMenu = document.querySelector('#main-menu');
 toogleMenu.addEventListener('click', function() {
+    document.body.classList.toggle("cross");
     const open = JSON.parse(toogleMenu.getAttribute('aria-expanded'));
     toogleMenu.setAttribute('aria-expanded', !open);
     mainMenu.hidden = !mainMenu.hidden;
